3f8a5371b8c14ed776a442adba0e18c2f059ef6f
grosarydasan <giovannirosarydasan@gmail.com>
2020-05-26 12:37:16 +0530

    executable tests

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import data.LoginData;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -12,16 +13,20 @@ import utils.JsonReaderHelper;
 import java.io.FileNotFoundException;
 import java.util.concurrent.TimeUnit;
 
+import static org.testng.Assert.fail;
+
 public class LoginTest {
     WebDriver driver;
     LoginPage loginPage;
     HomePage homePage;
     LoginData loginData;
 
+
     @BeforeTest
     public void setUp() throws FileNotFoundException {
         loginData = JsonReaderHelper.getLoginData()[0];
-        System.setProperty("webdriver.gecko.driver","src\\main\\resources\\Drivers\\geckodriver.exe");
+        System.setProperty("webdriver.gecko.driver", "src\\main\\resources\\Drivers\\geckodriver.exe");
+        driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.get("http://demo.guru99.com/V4/");
     }
@@ -35,6 +40,7 @@ public class LoginTest {
         Assert.assertTrue(loginPageTitle.toLowerCase().contains(loginData.getTitle()));
         loginPage.login(loginData.getUsername(),loginData.getPassword());
         Assert.assertTrue(homePage.getHomePageUsername().toLowerCase().contains(loginData.getUsername()));
+        System.out.println("the user name is expected as "+ loginData.getUsername());
     }
 
     @AfterTest
